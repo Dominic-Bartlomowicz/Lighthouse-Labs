547590c58072fe70c0d15a33d06bdb656ea1fd82
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-03-18 15:35:28 -0700

    first commit

---
+++ wkd1/readfileArgv.js
@@ -0,0 +1,15 @@
+var data = process.argv;
+var input = data[2];
+
+var fs = require("fs");
+
+fs.readFile(input, function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Asynchronous read: " + data.toString());
+});
+
+
+// Testing function: type node input.txt in the
+//                   console and verify output.
