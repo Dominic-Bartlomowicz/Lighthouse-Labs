e7526d4bfcb1393b598fb965a46087ed9ba6212e
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-03-19 12:14:30 -0700

    first commit

--- week2/w2d1/httpRequests/printinchunks.js
+++ week2/w2d1/httpRequests/printinchunks.js
@@ -27,3 +27,36 @@ function getAndPrintHTMLChunks () {
 }
 
 getAndPrintHTMLChunks();
+
+
+
+
+function getAndPrintHTML () {
+
+  var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk Received. Printing on a new line:', data.concat("\n"));
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  })
+}
+
+getAndPrintHTML();
